dabf8bd1a66e906386d34298937341b7f550a3eb
Sabda PS <sabdaps@gmail.com>
2024-03-16 19:28:01 +0700

    starting web

--- fspe-alfa/one.fsx
+++ fspe-alfa/one.fsx
@@ -1,24 +1,36 @@
-module One 
+module One
 
-printfn "Hello world"
+// This is a function that accepts a number and returns a list of digits of the number
+let numcol (num : int64) =
+  let rec numccol' (num : int64) (res : int64 list) =
+    match num with
+    | 0L -> res
+    | _ -> numccol' (num / 10L) (num % 10L :: res)
+  numccol' num []
 
-// ini comment bukan?
-let x = 10
-let y = 20
-let sum = x + y
-printfn "The sum of %d and %d is %d" x y sum
+// Create a function that accepts a number and check whether it is a prime number
+let isPrime (num : int64) =
+  let rec isPrime' (num : int64) (i : int64) =
+    match i with
+    | _ when i * i > num -> true
+    | _ when num % i = 0L -> false
+    | _ -> isPrime' num (i + 1L)
+  isPrime' num 2L
 
-// Gue butuh elo bikin prime-checking function here
+// Gue mau bikin function yg inputnya lim, and return the list of primes that are less than lim
+// sekalian return juga jumlah all primes yg kurang dari lim
+// pake sieve ya, pake array
+// salah lo, gue pengen yang returnnya tuh list of primes less than lim, and then jumlahnya
 
-let isPrime n =
-  if n < 2 then
-    false
-  else
-    let rec isDivisibleBy k =
-      if k * k > n then
-        true
-      else if n % k = 0 then
-        false
-      else
-        isDivisibleBy (k + 1)
-    isDivisibleBy 2
+
+
+// Gue mau bikin function untuk timing the function call and print to console the time it takes to execute the function
+let time (f : 'a -> 'b) (x : 'a) =
+  let sw = System.Diagnostics.Stopwatch()
+  sw.Start()
+  let res = f x
+  sw.Stop()
+  printfn "Elapsed time: %A" sw.Elapsed
+  res
+
+
